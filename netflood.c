1e0abdaacb9fa9606941499b560ed2b4d1f72a0d
Sijmen J. Mulder <ik@sjmulder.nl>
2018-03-02 00:13:53 +0100

    Connect and read/write data

--- netflood.c
+++ netflood.c
@@ -53,28 +53,71 @@ writeall(int fd, char *data, size_t len)
 	}
 }
 
+static void
+forward(int fdin, int fdout)
+{
+	ssize_t	nread;
+	char	buf[4096];
+
+	while (1) {
+		nread = read(fdin, buf, sizeof(buf));
+		if (nread == -1)
+			err(1, NULL);
+		if (nread == 0)
+			break;
+
+		writeall(fdout, buf, nread);
+	}
+}
+
 int
 main(int argc, char **argv)
 {
 	char		*input;
 	size_t		 inputlen;
 	struct addrinfo	 hints;
-	struct addrinfo	*addrs;
+	struct addrinfo	*addr0, *addr;
 	int		 error;
+	int		 sock	= -1;
 
 	if (argc != 3)
 		errx(1, "usage: netflood [host] [port | service]");
-	
+
 	readall(STDIN_FILENO, &input, &inputlen);
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_protocol = IPPROTO_TCP;
 
-	error = getaddrinfo(argv[1], argv[2], &hints, &addrs);
+	error = getaddrinfo(argv[1], argv[2], &hints, &addr0);
 	if (error)
 		errx(1, "lookup failed: %s", gai_strerror(error));
 
-	freeaddrinfo(addrs);
+	for (addr = addr0; addr; addr = addr->ai_next) {
+		sock = socket(addr->ai_family, addr->ai_socktype,
+		    addr->ai_protocol);
+		if (sock == -1) {
+			warn("socket()");
+			continue;
+		}
+
+		if (connect(sock, addr->ai_addr, addr->ai_addrlen) == -1) {
+			warn("connect()");
+			close(sock);
+			sock = -1;
+			continue;
+		}
+
+		break;
+	}
+
+	if (sock == -1)
+		errx(1, "failed to connect");
+
+	writeall(sock, input, inputlen);
+	forward(sock, STDOUT_FILENO);
+
+	close(sock);
+	freeaddrinfo(addr0);
 	return 0;
 }
